01870eb852c36c4a2b5f921c6eaf140f8b6aad62
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-02 16:25:00 +0200

    albums sorted

--- js/script.js
+++ js/script.js
@@ -7,14 +7,27 @@ const app = new Vue({
   data: {
     musics: '',
   },
-  methods: {},
+  methods: {
+    compare(a, b) {
+      if (a.year < b.year) {
+        return -1;
+      }
+      if (a.year > b.year) {
+        return 1;
+      }
+      return 0;
+    },
+
+  },
   created() {
     axios
       .get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((res) => {
         const result = res.data.response;
         this.musics = result;
+        result.sort(this.compare);
         console.log(this.musics);
+
       });
   }
 });
