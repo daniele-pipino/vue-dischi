7d2c954510f42b844695a473a4af1ac53ec84046
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-02 16:13:50 +0200

    render albums complete

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    musics: '',
+  },
   methods: {},
+  created() {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((res) => {
+        const result = res.data.response;
+        this.musics = result;
+        console.log(this.musics);
+      });
+  }
 });
